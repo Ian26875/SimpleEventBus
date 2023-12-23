d2fd5c9f8940a0fbabfee994c77fd954e86e7bc8
Ian <fds26875@gmail.com>
2023-12-23 20:33:10 +0800

    feat(SubscriptionDescriptor.cs): 建立 SubscriptionDescriptor

---
+++ SimpleEventBus/SubscriptionDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace SimpleEventBus;
+
+[DebuggerDisplay("EventType={EventType}，EventHandlerType={EventHandlerType}")]
+public class SubscriptionDescriptor
+{
+    public Type EventType { get; }
+
+    public Type EventHandlerType { get; }
+    
+
+    public SubscriptionDescriptor(Type eventType, Type eventHandlerType)
+    {
+        EventType = eventType;
+        EventHandlerType = eventHandlerType;
+    }
+}
