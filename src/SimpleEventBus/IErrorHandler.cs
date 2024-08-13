793adc5930fc4dc1616dc924b677784c6a6e8be3
Ian <fds26875@gmail.com>
2024-08-13 20:50:59 +0800

    Add XML documentation to IErrorHandler interface
    This commit adds XML comments to the IErrorHandler interface for better code documentation and clarity. The summary and parameter tags describe the interface and its method parameters, enhancing code readability and maintainability.

--- src/SimpleEventBus/IErrorHandler.cs
+++ src/SimpleEventBus/IErrorHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace SimpleEventBus;
+namespace SimpleEventBus;
 
+/// <summary>
+/// The error handler interface
+/// </summary>
 public interface IErrorHandler
 {
-    
+    /// <summary>
+    /// Ons the exception using the specified exception context
+    /// </summary>
+    /// <param name="exceptionContext">The exception context</param>
+    void OnException(ExceptionContext exceptionContext);
 }
