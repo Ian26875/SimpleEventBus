43d8177fa447fc8aa58f60c59a218439e00e1318
Ian <fds26875@gmail.com>
2024-08-19 11:35:21 +0800

    Add XML documentation to DefaultEventHandlerResolver
    Added summary and parameter annotations for the DefaultEventHandlerResolver class and its methods. This improves code readability and provides better context for developers using or maintaining the code.

--- src/SimpleEventBus/DefaultEventHandlerResolver.cs
+++ src/SimpleEventBus/DefaultEventHandlerResolver.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace SimpleEventBus;
 
+/// <summary>
+/// The default event handler resolver class
+/// </summary>
+/// <seealso cref="IEventHandlerResolver"/>
 public class DefaultEventHandlerResolver : IEventHandlerResolver
 {
+    /// <summary>
+    /// The service provider
+    /// </summary>
     private readonly IServiceProvider _serviceProvider;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DefaultEventHandlerResolver"/> class
+    /// </summary>
+    /// <param name="serviceProvider">The service provider</param>
     public DefaultEventHandlerResolver(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
     }
 
+    /// <summary>
+    /// Gets the handlers for event using the specified event
+    /// </summary>
+    /// <typeparam name="TEvent">The event</typeparam>
+    /// <param name="@event">The event</param>
+    /// <returns>The handlers</returns>
     public IEnumerable<IEventHandler<TEvent>> GetHandlersForEvent<TEvent>(TEvent @event) where TEvent : class
     {
         using var serviceScope = _serviceProvider.CreateScope();
