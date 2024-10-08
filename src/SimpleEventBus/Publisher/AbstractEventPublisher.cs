e156307ad303e76e0483f6388f3f4239c46971fb
Ian <fds26875@gmail.com>
2024-10-08 17:16:48 +0800

    Add XML documentation to AbstractEventPublisher class
    This adds descriptive XML comments to the AbstractEventPublisher class and its members to improve code readability and maintainability. These comments provide clear explanations of the class, constructor, and methods, aiding other developers in understanding the code.

--- src/SimpleEventBus/Publisher/AbstractEventPublisher.cs
+++ src/SimpleEventBus/Publisher/AbstractEventPublisher.cs
@@ -4,18 +4,41 @@ using SimpleEventBus.Serialization;
 
 namespace SimpleEventBus;
 
+/// <summary>
+/// The abstract event publisher class
+/// </summary>
+/// <seealso cref="IEventBus"/>
 public abstract class AbstractEventPublisher : IEventBus
 {
+    /// <summary>
+    /// The serializer
+    /// </summary>
     protected readonly ISerializer _serializer;
 
+    /// <summary>
+    /// The schema registry
+    /// </summary>
     protected readonly ISchemaRegistry _schemaRegistry;
     
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AbstractEventPublisher"/> class
+    /// </summary>
+    /// <param name="serializer">The serializer</param>
+    /// <param name="schemaRegistry">The schema registry</param>
     protected AbstractEventPublisher(ISerializer serializer, ISchemaRegistry schemaRegistry)
     {
         _serializer = serializer;
         _schemaRegistry = schemaRegistry;
     }
 
+    /// <summary>
+    /// Publishes the event
+    /// </summary>
+    /// <typeparam name="TEvent">The event</typeparam>
+    /// <param name="@event">The event</param>
+    /// <param name="headers">The headers</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    /// <exception cref="ArgumentNullException"></exception>
     public Task PublishAsync<TEvent>(TEvent @event, Headers? headers = null,
                                      CancellationToken cancellationToken = default(CancellationToken)) where TEvent : class
     {
@@ -38,6 +61,11 @@ public abstract class AbstractEventPublisher : IEventBus
         return this.PublishEventAsync(eventData, cancellationToken);
     }
 
+    /// <summary>
+    /// Publishes the event using the specified event data
+    /// </summary>
+    /// <param name="eventData">The event data</param>
+    /// <param name="cancellationToken">The cancellation token</param>
     protected abstract Task PublishEventAsync(EventData eventData, CancellationToken cancellationToken = default(CancellationToken));
 
 }
